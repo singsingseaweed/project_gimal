63dfc295b79b37af1f751a048b8d395c3bbbc5a6
singsingseaweed <sson408@naver.com>
2023-06-10 13:29:28 +0900

    Initial commit

---
+++ pages/1_page.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+
+st.set_page_config(
+    page_title="제어공학기말",
+)
+
+st.header("제어공학기말 202021025 임수창")
+st.subheader("2번문제 streamlit 으로 공유")
