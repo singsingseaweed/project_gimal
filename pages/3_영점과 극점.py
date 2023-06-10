84583bd1377a80b2849677f1f7d435a450e2985f
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2023-06-10 13:51:31 +0900

    3_영점과 극점.py

---
+++ pages/3_영점과 극점.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+
+st.set_page_config(
+    page_title="제어공학기말",
+)
+
+st.header("제어공학기말 202021025 임수창")
+st.subheader("2번문제 streamlit 으로 공유")
+
+
+num = [100]
+den = [1, 5, 106]
+
+zeros, poles, _ = signal.tf2zpk(num, den)
+
+fig, ax = plt.subplots()
+ax.plot(np.real(zeros), np.imag(zeros), 'o', label='Zeros')
+ax.plot(np.real(poles), np.imag(poles), 'x', label='Poles')
+ax.set(xlabel='Real', ylabel='Imaginary', title='Pole-Zero Plot')
+ax.grid(True)
+ax.legend()
+
+st.pyplot(fig)
