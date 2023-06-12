24391a3c6c50dc960ba3a4f1d29734dd54dfba9f
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2023-06-12 23:36:11 +0900

    Create 5_입력과 출력의 시간에 따른 변화.py

---
+++ pages/5_입력과 출력의 시간에 따른 변화.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import TransferFunction, lsim
+
+def plot_input_output():
+    num = [100]
+    den = [1, 5, 106]
+
+    G = TransferFunction(num, den)
+
+    t = np.linspace(0, 10, 500)
+
+    u = np.sin(t)
+
+    t, y, _ = lsim(G, u, t)
+
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(t, u, label='Input(sinusoidal)', color='blue')
+
+    plt.plot(t, y, label='Output', color='red')
+
+    plt.title('Input & Output Over Time')
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.grid()
+    plt.legend()
+
+    st.pyplot(plt.gcf())
+
+st.title('Input & Output Plot')
+st.write('This app demonstrates the input and output plot of a system over time.')
+
+plot_input_output()
